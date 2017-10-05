60881deb68520ecbfb1c2ca522958ef1e8c648e8
Dusty <bsdpunk@gmail.com>
2017-10-04 22:36:14 -0500

    yeah

--- three.sh
+++ three.sh
@@ -28,8 +28,8 @@ set -o nounset                              # Treat unset variables as an error
    #PN=$( echo $BROAD |  grep -o '^[0-9]\{7,8\}' )
    #PN=$(head -n1 <(grep -o '^[0-9]\{7,8\}' b.txt))
    #head -n1 <(grep 'A\.' <(clear;cat xa$ibk | grep -A1 'A\.'))) ;
-   NAME=$( grep "$n" h.txt -A2 -B2 |  grep '[A-Z]\.'  | grep '[A-Z]\. [A-Z -]\+[a-z]' -o  | gsed 's/  //g' | gsed 's/A\. //' | cut -d' ' -f1-4 | head -n1 | sed 's/^[A-Z]\. //')
-   HANDLE=$( grep "$n" h.txt -A2 -B2 | grep '[A-Z]\.'  | grep '[A-Z]\. [A-Z -]\+[a-z]' -o  | gsed 's/  //g' | gsed 's/A\. //' | cut -d' ' -f1-4 | head -n1 | sed 's/^[A-Z]\. //' | tr '[A-Z]' '[a-z]' | sed 's/ /-/g' | sed "s/$/-${n}/" )
+   NAME=$( grep "$n" h.txt -A2 -B2 |  grep '[A-Z]\.'  | grep '[A-Z]\. [A-Z -]\+[a-z]' -o  | gsed 's/  //g' | gsed 's/A\. //' | cut -d' ' -f1-4 | head -n1 | sed 's/^[A-Z]\. //' | sed 's/[A-Z][a-z]//')
+   HANDLE=$( grep "$n" h.txt -A2 -B2 | grep '[A-Z]\.'  | grep '[A-Z]\. [A-Z -]\+[a-z]' -o  | gsed 's/  //g' | gsed 's/A\. //' | cut -d' ' -f1-4 | head -n1 | sed 's/^[A-Z]\. //' | tr '[A-Z]' '[a-z]' | sed 's/ /-/g' | sed "s/$/-${n}/"| sed 's/[A-Z][a-z]//' )
    DESCRIPTION=$( grep "$n" b.txt  -B1 | grep '[A-E]\.' | sed 's/^[A-Z]\. //' | gsed 's/^[A-Z ]\+\([A-Z][a-z]\)/\1/' | sed 's/"//g')
    #grep 'A\.' b.txt -A2 -B2 | grep '^A.*\.$' -A2 | grep "$PN" -B2 | head -n1)
    #NAME=$(grep 'A\.' b.txt | grep 'A\. [A-Z -]\+[a-z]' -o  | sed 's/  //g'| sed 's/A\. //' | cut -d' ' -f1-4 )
